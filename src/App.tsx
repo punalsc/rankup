bae7297a15f1118f4630102308f1242ad4f89b1e
Punal Chotrani <punalsc@gmail.com>
2020-04-08 00:50:56 +0100

    managing catch promise

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,41 @@ import SubmitButton from './Components/SubmitButton';
 
 const App = () => {
 	const isInitialMount = useRef(true);
-	const [character, setCharacter] = useState();
+
+	const [character, setCharacter] = useState([]);
+	const [loading, setLoading] = useState(false);
 	const [inputValue, setInputValue] = useState('');
+	const [isError, setIsError] = useState(false);
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setInputValue(e.target.value);
 	};
 
+	const handleSubmit = (e: ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		setInputValue('');
+	};
+
 	const baseUrl = `https://gateway.marvel.com:443/v1/public/characters?name=${inputValue}&limit=100&ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b`;
 
 	useEffect(() => {
-		if (isInitialMount.current) {
-			isInitialMount.current = false;
-		} else {
-			const fetchData = async () => {
-				const res = await fetch(baseUrl);
-				res.json().then((res) => setCharacter(res));
-			};
-			fetchData();
-		}
+		const fetchData = async () => {
+			setLoading(true);
+			const res = await fetch(baseUrl);
+			res
+				.json()
+				.then((res) => {
+					setCharacter(res.data.results);
+					setLoading(false);
+				})
+				.catch((err) => {
+					if (err.message) {
+						setIsError(true);
+					}
+				});
+		};
+		isInitialMount.current ? (isInitialMount.current = false) : fetchData();
 	}, [inputValue, baseUrl]);
-
-	console.log(character !== undefined ? character : null);
 
 	return (
 		<div className='App'>
@@ -37,29 +50,45 @@ const App = () => {
 				<div className='row'>
 					<div className='one-full column'>
 						<section className='spacing-top'>
-							<Form
-								onSubmit={(e?: any) => {
-									e.preventDefault();
-								}}
-							>
+							<div className='row'>
+								<Form onSubmit={(e?: any) => handleSubmit(e)}>
+									<div className='row'>
+										<Input
+											onChange={(e?: any) => handleChange(e)}
+											value={inputValue}
+											placeholder='Enter character'
+										/>
+									</div>
+									<div className='row'>
+										<SubmitButton
+											placeholder='Enter username'
+											className='button-primary'
+											value='Submit'
+											onClick={() => {
+												console.log('Submit button clicked');
+											}}
+										/>
+									</div>
+								</Form>
+							</div>
+							{isError && (
 								<div className='row'>
-									<Input
-										onChange={(e?: any) => handleChange(e)}
-										value={inputValue}
-										placeholder='Enter character'
-									/>
+									<p>Something went wrong ...</p>
 								</div>
-								<div className='row'>
-									<SubmitButton
-										placeholder='Enter username'
-										className='button-primary'
-										value='Submit'
-										onClick={() => {
-											console.log('Submit button clicked');
-										}}
-									/>
-								</div>
-							</Form>
+							)}
+							<div className='row'></div>
+						</section>
+						<section>
+							<div className='row'>
+								{loading ? (
+									<p>Loading...</p>
+								) : (
+									character &&
+									character.map((item: any) => (
+										<a href={item.name}>{item.name}</a>
+									))
+								)}
+							</div>
 						</section>
 					</div>
 				</div>
