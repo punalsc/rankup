3823d499083371826b25509f7b4b2edf60524315
Punal Chotrani <punalchotrani@Punals-MBP.broadband>
2020-04-14 00:26:34 +0100

    Global context and updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import MainProvider from "./Providers/MainProvider";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Marvel data fetch/i);
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <MainProvider>
+      <App />
+    </MainProvider>
+  );
+  const linkElement = getByText(/Marvel Character Search/i);
   expect(linkElement).toBeInTheDocument();
 });
