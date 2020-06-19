545759002de783928bdcfc6f6daeb11e6078d405
Punal Chotrani <punalchotrani@Punals-MBP.broadband>
2020-06-19 16:29:07 +0100

    Added function to retrieve image from second api

--- server/server.js
+++ server/server.js
@@ -8,11 +8,16 @@ const axios = require("axios");
 // GraphQL Schema
 const schema = buildSchema(`
     type Query {
-        character(name: String!): Character
+        character(name: String!): Character,
+        image(name:String!): [Image]
     }
 
     type Character {
       description: String
+    }
+
+    type Image {
+      url: String
     }
 `);
 
@@ -22,32 +27,36 @@ const fetchDescription = async (args) => {
       `https://gateway.marvel.com:443/v1/public/characters?name=${args.name}&limit=100&ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b`
     );
 
+    console.log(res.data.data.results[0]);
     return res.data.data.results[0];
   } catch {
     (err) => err;
   }
 };
 
-// const fetchRelatives = async (args) => {
-//   try {
-//     const res = await axios.get(
-//       `https://superheroapi.com/api/10158405947604808/search/${args.name}`
-//     );
-//     return res.data.results.forEach((publisher) => {
-//       if (publisher.biography.publisher === "Marvel Comics") {
-//         res.results.forEach((found) => {
-//           if (found.name.toLowerCase() === name.toLowerCase()) {
-//             return found.biography["first-appearance"];
-//           }
-//         });
-//       }
-//     });
-//   } catch {}
-// };
+const fetchImage = async (args) => {
+  try {
+    const res = await axios.get(
+      `https://superheroapi.com/api/10158405947604808/search/${args.name}`
+    );
+
+    const { results } = await res.data;
+
+    return results.forEach((obj) => {
+      if (obj.biography.publisher === "Marvel Comics") {
+        console.log(obj.image);
+        return obj.image;
+      }
+    });
+  } catch {
+    (err) => err;
+  }
+};
 
 // Root resolver
-const root = {
+const rootValue = {
   character: fetchDescription,
+  image: fetchImage,
 };
 
 const app = express();
@@ -59,7 +68,7 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    rootValue: root,
+    rootValue,
     graphiql: true,
   })
 );
